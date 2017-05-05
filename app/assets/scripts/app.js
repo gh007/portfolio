5d797b48c5f1949d6cfdc9fddcd53959251098b5
Graham <ghemailme@yandex.com>
2017-05-05 15:34:01 +0700

    Compleated information about header

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+import from "modules/Navbar.js"
+
+
+ 
---
+++ app/assets/scripts/modules/Navbar.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+
+$(function(){
+  $(window).scroll(function() {
+     if($(window).scrollTop() >= 100) {
+       $('nav').addClass('scrolled');
+     }
+    else {
+      $('nav').removeClass('scrolled');
+    }
+  });
+});
+
+
+
+
+
+
+
+
