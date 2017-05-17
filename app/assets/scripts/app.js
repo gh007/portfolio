1a0184f7359e4faa4d9e8f70b873497962a9910f
Graham <ghemailme@yandex.com>
2017-05-17 08:42:33 +0700

    Completed sticky header and some finishing touches.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,7 @@
+import $ from 'jquery';
 import MobMenu from './modules/MobMenu';
+import StickyHeader from './modules/StickyHeader';
+
 
 var mobMenu = new MobMenu();
+var stickyHeader = new StickyHeader();
